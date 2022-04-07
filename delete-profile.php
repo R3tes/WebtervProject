5915584da161ff62812b63d5878836fefe6e3817
a-bakk <abelbakk@gmail.com>
2022-04-07 18:01:06 +0200

    mostmar pfp-t is torli

--- delete-profile.php
+++ delete-profile.php
@@ -16,9 +16,19 @@
     if (($key = array_search($torlendoFelhasznalo, $felhasznalok)) !== false) {
         unset($felhasznalok[$key]);
         adatokMentese("data/felhasznalok.txt", $felhasznalok); 
+
+        $engedelyezettKiterjesztesek = ["png", "jpg", "jpeg"];
+        $utvonal = "assets/img/profile-pictures/" . $_SESSION["user"]->getFelhasznalonev();
+
+        foreach ($engedelyezettKiterjesztesek as $kit) {
+            if (is_file("$utvonal.$kit")) {
+                unlink("$utvonal.$kit");
+            }
+        }
+
         session_unset();
         session_destroy();
-        header("Location: bejelentkezes.php.php");
+        header("Location: bejelentkezes.php");
     }
     else {
         header("Location: profile.php");
